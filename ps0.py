d8e0000c63ebb82390c70a9e89a591fe4cda73b0
david smith <dvdsmith84@gmail.com>
2019-11-11 12:06:02 +1000

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import math
+
+def power_log():
+    x = int(input("Enter number x: "))
+    y = int(input("Enter number y: "))
+    print(x**y)
+    print(math.log(x, y))
+    
+
+power_log()
